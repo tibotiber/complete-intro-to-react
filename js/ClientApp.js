813178aeb2c953bc02b25376288e747b24329598
Thibaut Tiberghien <thibaut@planecq.com>
2017-02-09 00:15:38 +0800

    switch to browser router

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
+import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
 
 const App = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div className='app'>
       <Match exactly pattern='/' component={Landing} />
       <Match pattern='/search' component={Search} />
     </div>
-  </HashRouter>
+  </BrowserRouter>
 )
 
 render(<App />, document.getElementById('app'))
